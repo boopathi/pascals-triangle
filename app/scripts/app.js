e21493de0e42334ab78ffc4482b280a45e1c3efe
Boopathi Rajaa <me@boopathi.in>
2014-04-12 18:12:21 +0530

    Sidebar is ready

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,17 @@
 'use strict';
 
-angular
-  .module('pascalApp', []);
+angular.module('pascal', []);
+
+var Pascal = function(n) {
+	this.grid = [];
+	this.n = n;
+};
+
+Pascal.prototype.fillGrid = function() {
+	this.grid = [];
+	var x = [1], i;
+	for(i in this.multiplexArray(x, this.n)) {
+		this.grid.push(x)
+		x = 
+	}
+};
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,10 +1,21 @@
 'use strict';
 
-angular.module('pascalApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('pascal').controller('MainCtrl', function($scope) {
+
+	$scope.currentView = 0;
+
+	$scope.selectView = function(i) {
+		$scope.currentView = i;
+	};
+
+	$scope.views = [
+		{ name: 'Primes' },
+		{ name: 'Powers of 2' },
+		{ name: 'Elevens' },
+		{ name: 'Hockey Stick Pattern' },
+		{ name: 'Triangular numbers' },
+		{ name: 'Square numbers' },
+		{ name: 'Fibonocci' },
+		{ name: 'Binomial expansion' },
+	];
+});
